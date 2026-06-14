969c48027e57e1b313150321f4972e07523d343f
midasbook <midasbook@users.noreply.github.com>
2026-06-15 08:44:37 +0900

    Connect poll to Google Sheets backend

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.PollConfig = {
-  googleAppsScriptUrl: "",
+  googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbxo9-kkCAxpZ54SIVr05ZDJYw1QcipVYG4vY35lhOpkTdzBtb0J0zG2HmjFi9zWPbHk/exec",
 };
